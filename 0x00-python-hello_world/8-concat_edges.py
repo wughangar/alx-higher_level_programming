614978098ad2d2e606a25aa982e297d04bbc78dc
wughangar <wughangar.rose@gmail.com>
2023-05-08 11:20:28 +0000

    completing source code

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str =f"{str[39:39+27]} {str[-22:-22+4]} {str[:6]}"
+print(str)
