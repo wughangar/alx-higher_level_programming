08caeb255a1ee7926943ab72ed0e00573da30eb5
wughangar <wughangar.rose@gmail.com>
2023-05-09 11:15:34 +0000

    change on print function

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for num in range(100):
-    print("{:02d}".format(num), end="")
     if num != 99:
-        print(", ", end="")
+        print("{:02d}, ".format(num), end="")
+    else:
+        print("{:02d}".format(num))
