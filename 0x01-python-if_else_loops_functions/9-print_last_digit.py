1f7c5dd34d3297d3ceafbd250f57f93c622eeefd
wughangar <wughangar.rose@gmail.com>
2023-05-09 14:19:16 +0000

    argh

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     ldigit = number % (10 if number >= 10 else -10)
+    ldigit *= -1 if number < 0 else 1
     print(ldigit, end='')
     return ldigit
