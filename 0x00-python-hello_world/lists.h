f316ae3155a28258dbc5f80036561f638e759fe9
wughangar <wughangar.rose@gmail.com>
2023-05-09 07:33:38 +0000

    linked list

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,1 +1,20 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer 
+ * @next : pointes to the next node
+ *
+ * Description: singly linked list node structure
+ */
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
 int check_cycle(listint_t *list);
+
+#endif /* LISTS_H */
