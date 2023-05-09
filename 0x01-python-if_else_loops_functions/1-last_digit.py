e9cd2b61c3850bc03f1e92c70c909c7a39448059
wughangar <wughangar.rose@gmail.com>
2023-05-09 10:41:04 +0000

    changes to handle negatives

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-anum = anum(number)
-ldigit = anum % 10
+ldigit = number % (10 if number >= 0 else -10)
 if ldigit > 5:
     print(f"Last digit of {number} is {ldigit} and is greater than 5")
 elif ldigit == 0:
