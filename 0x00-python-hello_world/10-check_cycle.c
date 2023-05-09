d81f00783e2db3f3fc6b18e1c74d9a365dd6df93
wughangar <wughangar.rose@gmail.com>
2023-05-09 07:23:40 +0000

    cycle detection

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,22 +1,27 @@
 #include "lists.h"
+#include <stdio.h>
 
+/**
+ * check_cycle - checks for cycles in linked lists
+ * @list: pointer to the linked list
+ *
+ * Return: 0 if no cycle and 1 if there is a cycle
+ */
 int check_cycle(listint_t *list)
 {
-	// Start with the first number
-	int num = list->n;
-	list = list->next;
+	listint_t *slw = list;
+	listint_t *fst = list;
 
-	while(list != NULL)
+	while (fst != NULL && fst-> != NULL)
 	{
-		if(list->n < num)
+		slw = slw->next;
+		fst = fst->next->next;
+
+		if (slw = fst)
 		{
-			return 1;
+			return (1);
 		}
-
-		num = list->n;
-		list = list->next;
 	}
-	
-	return 0;
-}
 
+	return (0);
+}
