a42f1cffe15465e972b148aa327e1e1fef9c10df
wughangar <wughangar.rose@gmail.com>
2023-05-09 07:40:44 +0000

    paranthesis added

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -12,12 +12,12 @@ int check_cycle(listint_t *list)
 	listint_t *slw = list;
 	listint_t *fst = list;
 
-	while (fst != NULL && fst-> != NULL)
+	while (fst != NULL && fst->next != NULL)
 	{
 		slw = slw->next;
 		fst = fst->next->next;
 
-		if (slw = fst)
+		if (slw == fst)
 		{
 			return (1);
 		}
