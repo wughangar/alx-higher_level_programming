0437e37bdf503975a2e971605316446c269b6265
wughangar <wughangar.rose@gmail.com>
2023-05-09 14:21:51 +0000

    Fix to 11

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -4,7 +4,16 @@ def pow(a, b):
         return 1
     elif b < 0:
         return 1 / pow(a, -b)
-    elif b % 2 == 0
-    return pow(a * a, b // 2)
+    elif b % 2 == 0:
+        return pow(a * a, b // 2)
     else:
         return a * pow(a, b -1)
+
+
+
+
+print(pow(2, 2))
+print(pow(98, 2))
+print(pow(98, 0))
+print(pow(100, -2))
+print(pow(-4, 5))
