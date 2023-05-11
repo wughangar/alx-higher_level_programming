b07c152e6bdbccad784d4963fde9f8b74fd99c5f
wughangar <wughangar.rose@gmail.com>
2023-05-11 13:28:41 +0000

    correct output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,8 +8,9 @@ if __name__ == "__main__":
             print("{} argument:".format(argc))
         if argc == 0:
             print(" 0 argument \n .")
+        if argc > 1:
+                print("{} arguments:".format(argc))
         else:
-            print("{} arguments:".format(argc))
             for i, j in enumerate(argv, start=1):
                 print("{}: {}".format(i, j))
 main()
