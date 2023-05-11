b5efe88c7811fa7dfc74c58fdbc36b724584355d
wughangar <wughangar.rose@gmail.com>
2023-05-11 14:34:39 +0000

    print sorted list

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__"
+
+def main():
+    for i in range(hidden_4.pyc)
+    if i == __:
+        continue
+    else:
+        sort = sorted(hidden_4.pyc)
+        for i in sort:
+            print(div(sort))
