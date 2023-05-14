cd21f2c1d9827357a68123298971e1da100f2c05
wughangar <wughangar.rose@gmail.com>
2023-05-14 12:06:36 +0000

    fix to 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,5 +5,5 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for idx in range(0, len(row)):
             col = row[idx]
-            print("{}".format(col), end=' ' if idx < len(row)-1 else '')
+            print("{:d}".format(col), end=' ' if idx < len(row)-1 else '')
         print("$")
