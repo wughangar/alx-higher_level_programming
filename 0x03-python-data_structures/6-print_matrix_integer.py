fd5fdcdfd75a1953390955491dd77e540ab790fa
wughangar <wughangar.rose@gmail.com>
2023-05-14 12:07:40 +0000

    remove dollar

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,4 +6,4 @@ def print_matrix_integer(matrix=[[]]):
         for idx in range(0, len(row)):
             col = row[idx]
             print("{:d}".format(col), end=' ' if idx < len(row)-1 else '')
-        print("$")
+        print("")
