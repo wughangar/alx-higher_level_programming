fe5add128fe7965d9cfd6a64a7dd619787f94459
wughangar <wughangar.rose@gmail.com>
2023-05-14 12:02:17 +0000

    Whoops

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,16 +7,3 @@ def print_matrix_integer(matrix=[[]]):
             col = row[idx]
             print("{}".format(col), end=' ' if idx < len(row)-1 else '')
         print("$")
-
-
-
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
-print("--$")
-print_matrix_integer()
