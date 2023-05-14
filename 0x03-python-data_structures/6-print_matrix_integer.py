73922a439fabe999b95cde5fe353878ada4cd843
wughangar <wughangar.rose@gmail.com>
2023-05-14 11:59:28 +0000

    fixes to 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for idx in range(0, len(row)):
+            col = row[idx]
+            print("{}".format(col), end=' ' if idx < len(row)-1 else '')
+        print("$")
