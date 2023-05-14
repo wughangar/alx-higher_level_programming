eddcd71c8a0a36aeb4eea5da5c3f3a1c28f22a72
wughangar <wughangar.rose@gmail.com>
2023-05-14 10:25:02 +0000

    string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     result_str = ''
     for ch in my_string:
-        if ch.lower() != 'c' or 'C':
+        if ch.lower() != 'c':
             result_str += ch
             return result_str
