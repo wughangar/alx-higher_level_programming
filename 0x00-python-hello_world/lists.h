9a24e4ceda795e63ea8d3d98c0b69a0cd0e9b51f
wughangar <wughangar.rose@gmail.com>
2023-05-15 16:11:13 +0000

    function that checks for cycle in linked list

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer 
@@ -15,6 +17,9 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+size_t print_listint(const listint_t *head);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
 #endif /* LISTS_H */
