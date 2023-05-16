fed15dce366b33404ef85af416c465b40ddb0297
wughangar <wughangar.rose@gmail.com>
2023-05-16 11:57:49 +0000

    function that checks for cycles in list

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -4,68 +4,6 @@
 #include <stddef.h>
 
 /**
- * print_listint: function to print the list
- * @head: pointer to head
- *
- * Return: number of nodes
- */
-
-size_t print_listint(const listint_t *head)
-{
-	int n;
-
-	while(head != NULL)
-	{
-		printf("%d\n", head->n);
-		head = head->next;
-		n++;
-	}
-	return (n);
-}
-
-/**
- * add_nodeint: adds new node at the beginning of the list
- * @head; pointer to head node 
- * @n; const in to be added
- *
- * Return: updatd listint_t
- */
-
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-	listint_t* new_node;
-
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->n = n;
-	new_node->next = *head;
-	*head = new_node;
-
-	return (new_node);
-}
-
-/**
- * free_listint_t; frees the list
- * @head: pointer to head node
- *
- * Return: none
- */
-
-void free_listint_t(listint_t *head)
-{
-	listint_t *current;
-
-	while(head != NULL)
-	{
-		current = head;
-		head = head->next;
-		free(current);
-	}
-}
-
-/**
  * check_cycle - checks for cycles in linked lists
  * @list: pointer to the linked list
  *
