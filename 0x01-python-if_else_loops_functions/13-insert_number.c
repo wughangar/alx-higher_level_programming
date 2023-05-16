f97cd8adbb02d9280f88ce1b030cb4336644ca5f
wughangar <wughangar.rose@gmail.com>
2023-05-16 12:28:53 +0000

    function that inserts node ina sorted singly linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -2,3 +2,42 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * insert_node: inserts number into sorted singly linked list
+ * @head: pointer to head 
+ * @number: number to be inserted
+ *
+ * Return: NULL if failed
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new_node = (listint_t*)malloc(sizeof(listint_t));
+	new_node->n = number;
+	new_node->next = NULL;
+
+	listint_t* current;
+
+	if (*head == NULL || (*head)->n >= new_node->n)
+	{
+		new_node->next = *head;
+		*head = new_node;
+	}
+	else
+	{
+		current = *head;
+		while (current->next != NULL && current->next-n < new_node->n)
+		{
+			current = current->next;
+		}
+		new_node->next = current->next;
+		current->next = new_node;
+
+		if (new_node == NULL)
+		{
+			return (NULL);
+		}
+	}
+		return (new_node)
+}
+
+
--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -10,12 +10,13 @@
 
 typedef struct Node
 {
-	int data;
+	int n;
 	struct Node* next;
 } listint_t;
 
-listint_t Cnode(int number);
-void printlist(listint_t* head);
-listint_t *insert_node(listint_t **head, int number);
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
 
+listint_t *insert_node(listint_t **head, int number);
 #endif /* LISTS-H */
