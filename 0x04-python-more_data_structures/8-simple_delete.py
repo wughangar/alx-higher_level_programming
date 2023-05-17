156fb9ff3e8e89c13a66b7555efd7fb2a3f72544
wughangar <wughangar.rose@gmail.com>
2023-05-17 10:39:28 +0000

    function that deletes the key

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def simple_delete(a_dictionary, key=""):
+    for x in a_dictionary:
+        if x != key
+        return a_dictionary
+    elif:
+        x == key
+        del a_dictionary[key]
