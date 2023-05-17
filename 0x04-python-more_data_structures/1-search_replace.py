d29eb64bc57ef3273edb9eeacf90c38bd25eff6d
wughangar <wughangar.rose@gmail.com>
2023-05-17 09:22:43 +0000

    function that replaces specific words in a list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list.replace("search", "replace")
+    new_list = []
+    for x in my_list:
+        if x == search:
+            new_listappend(replace)
+        else:
+            newlist.append(x)
+    return new_list
