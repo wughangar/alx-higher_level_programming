8375a66ff028698c1575ac894a17a882704b40e9
wughangar <wughangar.rose@gmail.com>
2023-05-17 17:39:44 +0000

    indentation

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,18 +4,20 @@
 def roman_to_int(roman_string):
     if roman_numeral is None or not isinstance(roman_string, str):
         return None
-    r_values = {'I': 1,
-                    'V': 5,
-                    'X': 10,
-                    'L': 50,
-                    'C': 100,
-                    'D': 500}
-    result = 0
-    rn_len = len(roman_string)
-    for i in range(len(roman_numeral)):
-        rom_val = roman_values[roman_numeral[i]]
-        if i < rn_len - 1 and rom_val < r_values[roman_numeral[i + 1]]:
-            reult -= rom_val
-        else:
-            result += rom_val
-    return result
+
+
+r_values = {'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500}
+result = 0
+rn_len = len(roman_string)
+for i in range(len(roman_numeral)):
+    rom_val = roman_values[roman_numeral[i]]
+    if i < rn_len - 1 and rom_val < r_values[roman_numeral[i + 1]]:
+        result -= rom_val
+    else:
+        result += rom_val
+return result
