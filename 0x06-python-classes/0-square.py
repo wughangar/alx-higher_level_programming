84b026b7bc00e2c2e50d6cd65e6cb2525d14620d
wughangar <wughangar.rose@gmail.com>
2023-05-30 10:32:01 +0000

    updated the documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -4,5 +4,7 @@
 class Square:
     """
     An empty class that defines a square.
-    """
 
+    This class can be used to create square objects and perform various
+    operations related to squares
+    """
