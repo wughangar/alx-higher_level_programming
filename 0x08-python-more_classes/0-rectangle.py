189e4fffc3d156db4218195d8e839a535c0bbfdb
wughangar <wughangar.rose@gmail.com>
2023-06-05 08:05:58 +0000

    readme file and an empty rectangle class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+This class cna be used to create rectangle objects and perform various operations related to the rectagle
+
+"""
+
+
+class Rectangle:
+    """
+    An empty class
+    """
+    pass
