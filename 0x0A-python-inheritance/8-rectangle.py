d2861bcbbb371d04c9d3a282b770aa85a8142943
wughangar <wughangar.rose@gmail.com>
2023-06-12 09:29:44 +0000

    corrected indentation

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -35,6 +35,7 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError(f"{name} must be greator than 0")
 
+
 class Rectangle(BaseGeometry):
     """
     child class with intitialize width and height
