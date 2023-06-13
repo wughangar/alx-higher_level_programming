5f04fbd95094f4ef73cba766d5d19173de5749c7
wughangar <wughangar.rose@gmail.com>
2023-06-13 07:57:16 +0000

    function tht creates an onject from a json gile

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import json
+"""
+6. Create object from a JSON file
+"""
+
+
+def load_from_json_file(filename):
+    """
+    function that creates an object from json file
+
+    Args:
+        filename: file to store the object file
+
+    """
+    with open(filename, "r") as f:
+        return json.load(f)
