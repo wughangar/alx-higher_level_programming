5e7295d7a5d3664efd274fa4f008ae54dd3483cc
wughangar <wughangar.rose@gmail.com>
2023-06-15 05:45:33 +0000

    files

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,4 +1,6 @@
 import unittest
+from ('6-max_integer.py') import max_integer
+
 """
 test for Max integer - Unittest
 """
@@ -19,9 +21,9 @@ class TestMaxinteger(unittest.TestCase):
         self.assertEqual(max_integer([-3, -20, -5, -1]), -1)
 
     def test_negative_positive(self):
-        self.assertEqual(max_integer([-1, 6. 2, 9]), 9)
+        self.assertEqual(max_integer([-1, 6.2, 9]), 9)
 
-    def test-floating_numbers(self):
+    def test_floating_numbers(self):
         self.assertEqual(max_integer([0.6, 4.8, 8.7]), 8.7)
 
     def test_non_value(self):
