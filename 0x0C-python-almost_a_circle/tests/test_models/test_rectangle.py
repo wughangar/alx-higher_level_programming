73e6d801cbd096538d39a4e3e536cef31ec28911
wughangar <wughangar.rose@gmail.com>
2023-06-19 16:02:15 +0000

    codestyle

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,17 +1,16 @@
+from unittest.mock import patch
+from models.rectangle import Rectangle
 from io import StringIO
 import unittest
 import sys
 sys.path.append('..')
-from unittest.mock import patch
-from models.rectangle import Rectangle
 
 """
 test cases for child class rectangle inheriting from base class
 """
 
+
 class TestRectangle(unittest.TestCase):
-    #test for tes_constructor method
-    #test for valid initialization values
     def test_constructor(self):
         r = Rectangle(5, 11, 3, 5)
         self.assertEqual(r.width, 5)
@@ -20,12 +19,10 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r.y, 5)
         self.assertEqual(r.id, 2)
 
-    #test for area
     def test_area(self):
         r = Rectangle(5, 11)
         self.assertEqual(r.area(), 55)
 
-    #test for display
     def test_display(self):
         r = Rectangle(5, 4)
         expected_output = "#####\n" \
@@ -36,7 +33,6 @@ class TestRectangle(unittest.TestCase):
             r.display()
             self.assertEqual(fake_output.getvalue(), expected_output)
 
-    #test for updates
     def test_update(self):
         r = Rectangle(5, 11, 3, 5)
         r.update(1, 7, 8, 4, 5)
@@ -50,14 +46,11 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r.width, 3)
         self.assertEqual(r.x, 1)
 
-    #test for str method
     def test_str(self):
         r = Rectangle(5, 10, 2, 3, 1)
         expected_output = "[Rectangle] (1)2/3 -5 / 10"
         self.assertEqual(str(r), expected_output)
 
+
 if __name__ == '__main__':
     unittest.main()
-
-
-
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,16 +1,16 @@
-import unittest
-import sys
-sys.path.append('..')
 from unittest.mock import patch
 from models.square import Square
 from io import StringIO
+import unittest
+import sys
+sys.path.append('..')
 
 """
 test cases for child class sqaure that inherits from rectangle
 """
 
+
 class TestSquare(unittest.TestCase):
-    #test fcase for constructor
     def test_constructor(self):
         square = Square(5, 2, 3, 1)
         self.assertEqual(square.size, 5)
@@ -20,12 +20,10 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(square.y, 3)
         self.assertEqual(square.id, 1)
 
-    #test for area
     def test_area(self):
         square = Square(5)
         self.assertEqual(square.area(), 25)
 
-    #test for update
     def test_update(self):
         square = Square(5)
         square.update(1, 7, 4, 5)
@@ -42,7 +40,6 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(square.height, 3)
         self.assertEqual(square.x, 1)
 
-    #test for str method
     def test_str(self):
         square = Square(5)
         square.update(1, 7, 4, 5)
@@ -59,7 +56,6 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(square.height, 3)
         self.assertEqual(square.x, 1)
 
-    #test for dictionary
     def test_to_dictionary(self):
         square = Square(5, 2, 3, 1)
         expected_dict = {
@@ -70,7 +66,6 @@ class TestSquare(unittest.TestCase):
         }
         self.assertEqual(square.to_dictionary(), expected_dict)
 
-    #test for display
     def test_display(self):
         square = Square(5)
         expected_output = "#####\n" \
@@ -81,6 +76,7 @@ class TestSquare(unittest.TestCase):
         with patch('sys.stdout', new=StringIO()) as fake_output:
             square.display()
             self.assertEqual(fake_output.getvalue(), expected_output)
+
 
 if __name__ == '__main__':
     unittest.main()
