d46006d09433593ddde8b7e5f3ba6abcc154c5aa
wughangar <wughangar.rose@gmail.com>
2023-07-18 16:27:46 +0300

    100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,7 +1,8 @@
 -- converting database into UTF8
 -- first modify data base encoding
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 collate utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- Modify table encoding
-ALTER TABLE first_table CHARACTER SET utf8mb4 collate utf8mb4_unicode_ci;
+USE hbtn_0c_0;
+ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- modify the columb encoding
-ALTER TABLE first_table MODIFY name CHARACTER SET utf8mb4 collate utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY name CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
