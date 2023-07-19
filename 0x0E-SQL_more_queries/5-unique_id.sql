598587785e3ed9ac629ce04219a4b9ad3b343b11
wughangar <wughangar.rose@gmail.com>
2023-07-19 14:50:44 +0300

    primary key

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
 -- script that creates unique_id with default, primary key constraints
 CREATE TABLE IS NOT EXISTS unique_id (
-	id INT DEFAULT 1 UNIQUE,
+	id INT DEFAULT 1 PRIMARY KEY,
 	name VARCHAR(256)
 );
