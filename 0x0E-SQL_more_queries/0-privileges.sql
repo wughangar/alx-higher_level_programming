aa444f7c2f2c52bbb6566dd6c53cebe0a95011b8
wughangar <wughangar.rose@gmail.com>
2023-07-19 12:46:29 +0300

    grants script

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,11 +1,5 @@
--- connect to mysql server
-mysql -u root -p
-
 -- script that lists the privilages of particular users in localhost
-SHOW GRANTS FOR 'user_0d_1'@localhost;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
 -- script to list grants for user user_0d_2
-SHOW GRANTS FOR 'user_0d_2'@localhost;
-
--- exit mysql
-EXIT
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
