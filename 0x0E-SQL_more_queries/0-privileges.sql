6ca90928f0fcc358deb4e16b15c6d0bf8a22ed7e
wughangar <wughangar.rose@gmail.com>
2023-07-19 12:32:00 +0300

    script that lists privilages to particular users

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- script that lists the privilages of particular users in localhost
+SHOW GRANTS FOR 'user_0d_1'@localhost;
+
+-- script to list grants for user user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@localhost;
