e44c37141e49dad6509d582a4c207d01d5ed7e22
wughangar <wughangar.rose@gmail.com>
2023-07-19 14:47:17 +0300

    code sayout change

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,5 @@
 -- script that creates unique_id with default, primary key constraints
-CREATE TABLE IS NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
+CREATE TABLE IS NOT EXISTS unique_id (
+	id INT DEFAULT 1 UNIQUE,
+	name VARCHAR(256)
+);
