8aaee34219fb061118947ef855ea4ec961050887
wughangar <wughangar.rose@gmail.com>
2023-07-20 09:57:15 +0300

    script that displays number of shows linked to each other

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- script that shows shows that are linked to each other
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.id, tv_genres.name
+ORDER BY number_of_shows DESC;
