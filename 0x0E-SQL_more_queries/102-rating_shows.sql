ebdc6cf74b636a78d68aa0bf5e36192cb3b9d10a
wughangar <wughangar.rose@gmail.com>
2023-07-20 14:41:03 +0300

    changed second alias

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -3,4 +3,4 @@ SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows
 JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
 GROUP BY tv_shows.title
-ORDER BY rating_sum DESC;
+ORDER BY rating DESC;
