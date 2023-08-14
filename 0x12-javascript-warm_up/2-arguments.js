d846e9e8a5d0094249ee858545a1217884820cef
wughangar <wughangar.rose@gmail.com>
2023-08-14 09:50:08 +0300

    Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+let args = process.argv.slice(2);
+
+if (args.length === 0) {
+	console.log("No arguments");
+} else if (args.length === 1) {
+	console.log("Agument found");
+} else {
+	console.log("Arguements found");
+}
