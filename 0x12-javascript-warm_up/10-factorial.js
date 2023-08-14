ff1d2b6069f33de51ad5b53447ba81f04870afaa
wughangar <wughangar.rose@gmail.com>
2023-08-14 13:52:28 +0300

    style

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,19 +4,18 @@ const arg = process.argv[2];
 let num;
 
 if (!isNaN(arg)) {
-	num = parseInt(arg);
+  num = parseInt(arg);
 } else {
-	console.log(1);
-	process.exit();
+  console.log(1);
+  process.exit();
 }
 
-function factorial(num) {
-	if (num === 0 || num === 1)
-	{
-		return 1;
-	} else {
-		return num * factorial(num - 1);
-	}
+function factorial (num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
 }
 
 const result = factorial(num);
