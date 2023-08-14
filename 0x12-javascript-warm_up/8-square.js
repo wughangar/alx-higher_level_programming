65978c27286406e415c613e96788fcb3a04377d8
wughangar <wughangar.rose@gmail.com>
2023-08-14 12:37:21 +0300

    script that prints a sqaure

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const arg = process.argv[2];
+let num = 0;
+
+if (!isNaN(arg)) {
+  num = parseInt(arg);
+} else {
+  console.log('Missing size');
+}
+
+for (let i = 0; i < num; i++) {
+	let row = 'X';
+	for (let j = 0; j < num; j++) {
+		row += 'X';
+	}
+	console.log(row);
+}
