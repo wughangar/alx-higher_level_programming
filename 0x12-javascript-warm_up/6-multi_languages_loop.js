e55d323ac54bf62ebd062d21ca68ed669bc4dcc9
wughangar <wughangar.rose@gmail.com>
2023-08-14 12:12:44 +0300

    spellings

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const myList = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const myList = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (const i of myList) {
   console.log(i);
