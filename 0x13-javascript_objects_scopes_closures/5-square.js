f7c813d869ab4aa861e4fc3066bd9640895be353
wughangar <wughangar.rose@gmail.com>
2023-08-15 13:06:29 +0300

    style

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 class Square extends require('./4-rectangle') {
-	constructor(size) {
-		super(size, size);
-		this.size = size;
-	}
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
 }
 
 module.exports = Square;
---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+
+class Square extends require('./5-square') {
+
