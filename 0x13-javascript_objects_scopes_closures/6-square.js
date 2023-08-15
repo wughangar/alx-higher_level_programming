a787ffd3f55abba327e8e8a1b8f7f48216a3cc17
wughangar <wughangar.rose@gmail.com>
2023-08-15 13:35:11 +0300

    lass Square that defines a square and inherits from Square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,4 +1,24 @@
 #!/usr/bin/node
 
 class Square extends require('./5-square') {
+  constructor (size) {
+    super(size);
+    this.size = size;
+  }
 
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += c;
+      }
+      console.log(row);
+    }
+  }
+}
+
+module.exports = Square;
