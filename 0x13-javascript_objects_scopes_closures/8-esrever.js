5fc36b97c55a08452c562daded9902518220d8e6
wughangar <wughangar.rose@gmail.com>
2023-08-15 19:41:26 +0300

    reversed list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  const newList = [];
+  const reversedList = [];
   let i;
   for (i = list.length - 1; i >= 0; i--) {
-    newList.push(list[i]);
+    reversedList.push(list[i]);
   }
-  return newList;
+  return reversedList;
 };
