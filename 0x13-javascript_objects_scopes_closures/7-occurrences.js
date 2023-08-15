7f0b1fb84a263ebe9f7452a3324c9e10f9a2cc4c
wughangar <wughangar.rose@gmail.com>
2023-08-15 13:50:33 +0300

    style

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 exports.nbOccurences = function (list, searchElement) {
-	let total = 0;
-	for (let i of list) {
-		if (i === searchElement) {
-			total++;
-		}
-	}
-	return total;
+  let total = 0;
+  for (const i of list) {
+    if (i === searchElement) {
+      total++;
+    }
+  }
+  return total;
 };
