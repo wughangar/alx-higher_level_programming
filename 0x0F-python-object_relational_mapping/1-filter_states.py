00f012c3804782333db1506bc74f2714b20514f1
wughangar <wughangar.rose@gmail.com>
2023-08-18 14:28:30 +0300

    Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+import sys for argv and mysqldb
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    """
+    script that lists all the states from the database hbtn_0e_0_usa
+    """
+
+    if len(sys.argv) != 4:
+        sys.exit("Usage: python script.py <mysql_username>"
+                 "<mysql_password> <database_name>")
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database_name
+            )
+    cursor = db.cursor()
+
+    selected_list = ("SELECT *  FROM states"
+                     "WHERE name LIKE 'N%' ORDER BY id ASC;")
+
+    cursor.execute(selected_list)
+
+    output = cursor.fetchall()
+    for row in output:
+        print(row)
+
+    cursor.close()
+    db.close()
