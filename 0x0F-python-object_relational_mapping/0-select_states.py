5161f46a3ef9be0cf945448678938f36c5409890
wughangar <wughangar.rose@gmail.com>
2023-08-18 13:05:18 +0300

    script that gets all the states from database

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+"""
+script that lists all the states from the database hbtn_0e_0_usa
+"""
+
+#get the required parameters
+username = sys.argv[1]
+password = sys.argv[2]
+database_name = sys.argv[3]
+
+#connect to database
+db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database_name)
+cursor = db.cursor()
+
+#send and execute the request
+selected_list = "SELECT *  FROM states ORDER BY id ASC;"
+
+cursor.execute(selected_list);
+
+#get the feedback and display it
+output = cursor.fetchall()
+for row in output:
+    print(row)
+
+#close the connection and cursor
+cursor.close()
+db.close()
