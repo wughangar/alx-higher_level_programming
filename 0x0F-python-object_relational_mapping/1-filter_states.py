911683a87f8dd8cbf4a889a21e9dc89698cc8fb3
wughangar <wughangar.rose@gmail.com>
2023-08-18 14:42:47 +0300

    filters states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     cursor = db.cursor()
 
     selected_list = ("SELECT *  FROM states "
-                     "WHERE name LIKE 'N%' " 
+                     "WHERE name LIKE 'N%' "
                      "ORDER BY id ASC;")
 
     cursor.execute(selected_list)
