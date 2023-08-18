1a33c2a14add1996002708390a9d7435da65bf24
wughangar <wughangar.rose@gmail.com>
2023-08-18 13:17:07 +0300

    corrected spaces

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
 
-import MySQLdb
-import sys
-"""
-script that lists all the states from the database hbtn_0e_0_usa
-"""
+if __name__ = '__main__':
+    import MySQLdb
+    import sys
+    """
+    script that lists all the states from the database hbtn_0e_0_usa
+    """
 
-# get the required parameters
-username = sys.argv[1]
-password = sys.argv[2]
-database_name = sys.argv[3]
+    # get the required parameters
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
 
-# connect to database
-db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database_name)
-cursor = db.cursor();
+    # connect to database
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database_name)
+    cursor = db.cursor();
 
-# send and execute the request
-selected_list = "SELECT *  FROM states ORDER BY id ASC;"
+    # send and execute the request
+    selected_list = "SELECT *  FROM states ORDER BY id ASC;"
 
-cursor.execute(selected_list)
+    cursor.execute(selected_list)
 
-# get the feedback and display it
-output = cursor.fetchall()
-for row in output:
-    print(row)
+    # get the feedback and display it
+    output = cursor.fetchall()
+    for row in output:
+        print(row)
 
-# close the connection and cursor
-cursor.close()
-db.close()
+    # close the connection and cursor
+    cursor.close()
+    db.close()
