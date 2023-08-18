a538dc09a66e3ca4c47f66d7b66817a68681e361
wughangar <wughangar.rose@gmail.com>
2023-08-18 14:41:08 +0300

    spaces corrected

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,10 +7,6 @@ import MySQLdb
 import sys
 
 if __name__ == '__main__':
-    """
-    script that lists all the states from the database hbtn_0e_0_usa
-    """
-
     if len(sys.argv) != 4:
         sys.exit("Usage: python script.py <mysql_username>"
                  "<mysql_password> <database_name>")
@@ -27,8 +23,8 @@ if __name__ == '__main__':
             )
     cursor = db.cursor()
 
-    selected_list = ("SELECT *  FROM states"
-                     "WHERE name LIKE 'N%'"
+    selected_list = ("SELECT *  FROM states "
+                     "WHERE name LIKE 'N%' " 
                      "ORDER BY id ASC;")
 
     cursor.execute(selected_list)
