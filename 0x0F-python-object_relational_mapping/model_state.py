ac8967becbf0f1c44597b6e39d3e3c9557274df8
wughangar <wughangar.rose@gmail.com>
2023-08-19 12:52:11 +0300

    documentation

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -11,6 +11,10 @@ Base = declarative_base()
 
 
 class State(Base):
+    """
+    state class that inherits from Base
+    class has id and name attributes
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
