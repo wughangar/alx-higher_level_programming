74fa9f02d6c023869d549782a4715fe7de62644f
wughangar <wughangar.rose@gmail.com>
2023-08-19 11:11:00 +0300

    model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -11,6 +11,6 @@ Base = declarative_base()
 
 
 class State(Base):
-    __table__name = 'states'
+    __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
