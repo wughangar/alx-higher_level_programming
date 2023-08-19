3979a8708bb6e2d4fbdfc53d5e94cc52db848013
wughangar <wughangar.rose@gmail.com>
2023-08-19 13:05:08 +0300

    spaving corrected

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -17,10 +17,10 @@ if __name__ == '__main__':
         print("Usage: ./{0} <{1}> <{2}> <{3}>".format(
             sys.argv[0], sys.argv[1], sys.argv[2], sys.argv[3]))
         sys.exit(1)
-        engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-                               sys.argv[1], sys.argv[2], sys.argv[3]),
-            pool_pre_ping=True)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                            sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
