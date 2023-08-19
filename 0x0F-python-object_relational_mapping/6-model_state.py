0b2ba0a526f484749ee4dc397cd3fd968d3ed742
wughangar <wughangar.rose@gmail.com>
2023-08-19 12:19:36 +0300

    port added

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -8,7 +8,7 @@ import sys
 from sqlalchemy import create_engine
 from model_state import Base, State
 if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]
         ), pool_pre_ping=True)
     Base.metadata.create_all(engine)
