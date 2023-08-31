f39f622edddfd8874097150be72b62ae513e9b9e
wughangar <wughangar.rose@gmail.com>
2023-08-31 12:28:17 +0300

    changed number of lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# bash scipt that takes url and displays size of the body 
-
-url="$1"
-response=$(curl -s "$url")
-body_size=$(echo -n "$response" | wc -c)
-echo "$body_size"
+# bash scipt that takes url and displays size of the body
+url="$1"; response=$(curl -s "$url"); echo -n "$response" | wc -c
