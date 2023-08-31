c0880cb82b618a85d0ee9ffb3b28b43ad59c01b7
wughangar <wughangar.rose@gmail.com>
2023-08-31 13:51:15 +0300

    added echo

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL and displays all HTTP methods the server will accept
-url="$1"; curl -s -I -X OPTIONS "$url" | grep "Allow" | awk '{print $2}'
+url="$1"; methods=$(curl -s -I -X OPTIONS "$url" | grep -i "Allow" | awk '{print $2}'); echo "$methods"
