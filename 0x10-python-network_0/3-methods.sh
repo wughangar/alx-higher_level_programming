138b7869b02c13e6063c4d83827d1ce670b3f122
wughangar <wughangar.rose@gmail.com>
2023-08-31 14:22:48 +0300

    delete method

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL and displays all HTTP methods the server will accept
-url="$1"; methods=$(curl -s -I -X OPTIONS "$url" | grep -i "Allow" | awk '{print $2}'); echo "$methods"
+curl -s -X DELETE $1
