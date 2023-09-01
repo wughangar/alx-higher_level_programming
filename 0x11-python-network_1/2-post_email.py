c78ad42d95d204455b1cbe0f9971c0369970a368
wughangar <wughangar.rose@gmail.com>
2023-09-01 11:55:15 +0300

    encode email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,7 +7,8 @@ import urllib.request
 import sys
 
 url = sys.argv[1]
-data = sys.argv[2]
+email = sys.argv[2]
+data = urllib.parse.urlencode({'email': email}).encode('utf-8')
 
 req =  urllib.request.Request(url, data=data, method="POST")
 with urllib.request.urlopen(req) as response:
