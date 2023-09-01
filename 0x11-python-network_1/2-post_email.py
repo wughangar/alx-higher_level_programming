6957397422f29e245b6b51753680b6973429050c
wughangar <wughangar.rose@gmail.com>
2023-09-01 14:51:13 +0300

    with added

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
     """
     url = sys.argv[1]
     email = sys.argv[2]
-    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-    req =  urllib.request.Request(url, data=data, method="POST")
+    data = with urllib.parse.urlencode({'email': email}).encode('utf-8')
+    req =  with urllib.request.Request(url, data=data, method="POST")
     with urllib.request.urlopen(req) as response:
         ans = response.read()
         decoded_ans = ans.decord('utf-8')
