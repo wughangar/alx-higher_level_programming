35bcde0086ab51f5249d7cd96ddf21b221142119
wughangar <wughangar.rose@gmail.com>
2023-09-01 12:59:26 +0300

    style changes

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,12 +6,15 @@
 import urllib.request
 import sys
 
-url = sys.argv[1]
-email = sys.argv[2]
-data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-
-req =  urllib.request.Request(url, data=data, method="POST")
-with urllib.request.urlopen(req) as response:
-    ans = response.read()
-    decorded_ans = ans.decord('utf-8')
-    print(decorded_ans)
+if __name__ == "__main__":
+    """
+    post an email
+    """
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    req =  urllib.request.Request(url, data=data, method="POST")
+    with urllib.request.urlopen(req) as response:
+        ans = response.read()
+        decorded_ans = ans.decord('utf-8')
+        print(decorded_ans)
--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,12 +6,12 @@ import urllib.request
 import urllib.error
 import sys
 
-url = sys.argv[1]
-
-try:
-    with urllib.request.urlopen(url) as response:
-        data = response.read()
-        decoded_data = data.decord('utf-8')
-        print(decoded_data)
-except urllib.error.HTTPError as e:
-    print(f"Error code: {e.code}")
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            data = response.read()
+            decoded_data = data.decord('utf-8')
+            print(decoded_data)
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
