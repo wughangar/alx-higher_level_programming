1baae9dfad84631942c97e82a7f0cdb2f6bb0a10
wughangar <wughangar.rose@gmail.com>
2023-09-01 15:24:34 +0300

    email changed

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,6 +8,6 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    data = {'email': email}
+    data = {'email': 'email'}
     req = requests.post(url, json=data)
     print(req.text)
