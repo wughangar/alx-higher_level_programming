d3875fda16335ac91208f097bea629e82b3f6df0
wughangar <wughangar.rose@gmail.com>
2023-09-01 14:52:21 +0300

    style

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     data = with urllib.parse.urlencode({'email': email}).encode('utf-8')
-    req =  with urllib.request.Request(url, data=data, method="POST")
+    req = with urllib.request.Request(url, data=data, method="POST")
     with urllib.request.urlopen(req) as response:
         ans = response.read()
         decoded_ans = ans.decord('utf-8')
