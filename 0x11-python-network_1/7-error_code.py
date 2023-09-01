99297dd273205f4ebe68db924720fd3cb0269b09
wughangar <wughangar.rose@gmail.com>
2023-09-01 16:57:24 +0300

    Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,6 +8,7 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     req = requests.get(url)
-    print(req.text)
     if req.status_code >= 400:
         print(f"Error code: {req.status_code}")
+    else:
+        print(req.text)
