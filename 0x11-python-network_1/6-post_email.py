9a7002a90b384582b25d6e40f7b4ed06ada33452
wughangar <wughangar.rose@gmail.com>
2023-09-01 15:30:15 +0300

    file

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     data = {'email': email}
-    req = requests.post(url, json=data)
-    print(f"Response body: {req.text}")
+    req = requests.post(url, data=data)
+    print(req.text)
