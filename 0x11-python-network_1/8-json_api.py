637699c3d03b6b257d018d1415d2e8fba552aa46
wughangar <wughangar.rose@gmail.com>
2023-09-01 14:48:21 +0300

    search api

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+8. Search API
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    letter = sys.argv[1] if len(sys.argv) > 1 else ""
+    url = "http://0.0.0.0:5000/search_user"
+    info = {"q": letter}
+
+    response = requests.post(url, data=info)
+    try:
+        data = response.json()
+        if data:
+            print(f"[{data['id']}] {data['name']}")
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
