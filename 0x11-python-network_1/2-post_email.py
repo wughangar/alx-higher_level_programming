86d2c072d13de6e2cfd062e885df018340301fc8
wughangar <wughangar.rose@gmail.com>
2023-09-01 11:46:49 +0300

    2. POST an email #0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+2. POST an email #0
+"""
+
+import urllib.request
+import sys.argv
+
+url = sys.argv[1]
+data = sys.argv[2]
+
+req =  urllib.request.Request(url, data=data, method="POST")
+with urllib.request.urlopen(req) as response:
+    ans = response.read()
+    decorded_ans = ans.decord('utf-8')
+    print(decorded_ans)
