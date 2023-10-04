b9cc144a90b3615ab00340b94da347a3d0824ad3
wughangar <wughangar.rose@gmail.com>
2023-10-04 10:33:45 +0300

    Toggle classes

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$("#toggle_header").click(function() {
+		const header = $("header");
+		if (header.hasClass("red")) {
+			header.removeClass("red").addClass("green");
+		} else {
+			header.removeClass("green").addClass("red");
+		}
+	});
+});
